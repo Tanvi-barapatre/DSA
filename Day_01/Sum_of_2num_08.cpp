67c1f4ad921cab02e71b932710f9985bbf15c07b
Tanvi-barapatre <tanvi.patrey18@gmail.com>
2025-08-01 00:32:46 +0530

    Add files via upload
    This commit includes a simple C++ program that takes two numbers as input from the user, calculates their sum, and displays the result. It demonstrates basic input/output operations and arithmetic addition.

---
+++ Day_01/Sum_of_2num_08.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, sum=0;
+    
+    cout << "Enter the value for 'a' : ";
+    cin >> a;
+    cout << "Enter the value for 'b' : ";
+    cin >> b;
+    sum = a + b;
+    cout << "The sum is : " << sum << endl;
+    return 0;
+}
