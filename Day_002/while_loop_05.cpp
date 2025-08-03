b398b87614f7711e35cf7255dc1bc69b4859fd6b
Tanvi-barapatre <tanvi.patrey18@gmail.com>
2025-08-03 22:45:08 +0530

    Add files via upload
    This program uses a while loop to compute the sum of all odd numbers from 1 to a given number n. It illustrates loop control with conditional checks and selective summation in C++.

---
+++ Day_002/while_loop_05.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+//While loop 
+
+//WAP to find the sum of all the ODD numbers from 1 to n using while loop.
+
+int main () {
+    int num, evenSum=0;
+    cout << "Enter the last number : ";
+    cin >> num;
+
+    int i=1;
+    while (i <= num) {
+        if (i % 2 == 0) {
+            evenSum+=i;
+        }
+        i++;
+    }
+    cout << "The sum of all the odd numbers is : " << evenSum << endl;
+    return 0;
+}
