435af6f047fa8918985583aaae61d075beddcc52
Tanvi-barapatre <tanvi.patrey18@gmail.com>
2025-08-03 22:41:29 +0530

    Add files via upload
    This program uses a while loop to print numbers from 1 to 5. It shows basic loop initialization, condition checking, and counter incrementing—ideal for beginners learning loop control in C++.

---
+++ Day_002/while_loop_01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//While loop
+
+//WAP to print numbers from 1 to 5.
+
+int main () {
+    int i=1;
+    while (i < 6) {
+        cout << i << endl;
+        i++ ;
+    }
+    return 0;
+} 
