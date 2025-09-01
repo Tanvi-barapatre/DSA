931b662336179a4f6544b8cce76cc02d35be01da
Tanvi-barapatre <tanvi.patrey18@gmail.com>
2025-09-01 21:12:01 +0530

    Add files via upload
    Added C++ file explaining nested loops with syntax and examples.

---
+++ Day_003/Nested_loops_00.cpp
@@ -0,0 +1,16 @@
+// What is a Nested Loop?
+// - A nested loop means using one loop inside another loop.
+// - The inner loop executes completely for each iteration of the outer loop.
+
+//  **Outer loop → controls the number of passes.
+
+//  **Inner loop → executes repeatedly for every outer loop pass.
+
+//SYNTAX
+
+// for(initialization; condition; update) {      // Outer loop
+//     for(initialization; condition; update) {  // Inner loop
+//         // Code to be executed
+//     }
+// }
+
