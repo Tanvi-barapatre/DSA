a822349ec47fb38193e09944018d19b574c9c254
Tanvi-barapatre <tanvi.patrey18@gmail.com>
2025-09-01 21:16:15 +0530

    Add files via upload
    Added C++ program to print n stars in m lines using nested loops.

---
+++ Day_003/Nested_loops_P1_01.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+//Patterns
+
+// WAP to print n number of stars in m number of lines.
+
+int main() {
+    int m, n;
+    cout << "Enter number of lines : ";
+    cin >> m;
+    cout << "Enter number of stars : ";
+    cin >> n;
+
+    for (int i = 1; i <= m; i++ ){      //lines (rows)
+        for (int j = 1; j <= n; j++) {  //stars (column)
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// Output :
+
+// Enter number of lines : 4
+// Enter number of stars : 5
+// * * * * * 
+// * * * * * 
+// * * * * * 
+// * * * * *
